652c2210d1c271599a4f8d0386e7af829e987d7a
Jared Kjar <jaredkjar@gmail.com>
2018-03-23 11:57:27 -0700

    wip

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, TextInput, ScrollView, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as axios from 'axios'
 const myIcon = (<Icon name="align-center" size={40} color="#fff" />)
 
 export default class App extends React.Component {
   state = {
     fontLoaded: false,
-    keyboardShown: null
+    keyboardShown: null,
+    username: '',
+    password: ''
   }
   async componentWillMount() {
     await Expo.Font.loadAsync({
@@ -14,6 +17,18 @@ export default class App extends React.Component {
       'Lato-Bold': require('./fonts/Lato-Bold.ttf'),
     });
     this.setState({ fontLoaded: true });
+  }
+
+  login = () => {
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    axios.post('http://192.168.1.112:53472/api/login', user)
+      .then(
+        resp => alert("loggin in!"),
+        err => console.log(err.data)
+      )
   }
 
   render() {
@@ -35,19 +50,20 @@ export default class App extends React.Component {
             onPress={() => {
               Alert.alert('You tapped the button!');
             }}
+            onChangeText={(text) => this.setState({username: text})}
           />
           <Text style={styles.label}>Password:</Text>
           <TextInput 
             style={styles.logIn}
             placeholder="Password"
             secureTextEntry={true}
-            
+            onChangeText={(text) => this.setState({password: text})}
           />
           <Button
             style={styles.buttons}
             title="Log In"
             color="white"
-            onPress={() => console.log('ok')}
+            onPress={() => this.login()}
           />
             <TouchableWithoutFeedback
               onPress={() => Keyboard.dismiss()}
